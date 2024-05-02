d7fa74e7b0e3b8f481808a500a4f0fe3073e66f1
HaidarAli54 <haidarali5464@gmail.com>
2024-05-02 05:55:37 -0700

    chapter4

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("Hello World!!")
+(async function () {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json = await response.json();
+    if (json.userId == 1) {
+      json.completed == false;
+    } else {
+      json.completed == true;
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+})();
